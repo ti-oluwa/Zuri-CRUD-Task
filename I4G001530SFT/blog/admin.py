2d7e972433f7903be0b7786009ea32921c06976a
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2022-06-23 19:45:46 +0100

    new commit

--- I4G001530SFT/blog/admin.py
+++ I4G001530SFT/blog/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-
-from I4G001530SFT import blog
-from I4G001530SFT.blog.models import Post
+from . models import Post
 
 # Register your models here.
 admin.site.register(Post)
